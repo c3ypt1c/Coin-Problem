bcbeb227daee381f6a265c338d9a460eac6ef5bb
c3ypt1c <c3ypt1c@gmail.com>
2016-07-22 23:09:54 +0100

    Finished product.

--- Coin-Problem/Coin-Problem/Form1.Designer.vb
+++ Coin-Problem/Coin-Problem/Form1.Designer.vb
@@ -22,9 +22,277 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Engine = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"1p", "2p", "5p", "10p", "20p", "50p", "£1", "£2"})
+        Me.ComboBox1.Location = New System.Drawing.Point(188, 63)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(59, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "1p"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(188, 24)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(59, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(189, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Coin Type:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(160, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Number of coins:"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(13, 229)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(169, 13)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "github.com/c3ypt1c/Coin-Problem"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(122, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Weight of the coins (mg)"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Increment = New Decimal(New Integer() {3, 0, 0, 0})
+        Me.NumericUpDown2.Location = New System.Drawing.Point(15, 25)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {10000000, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(91, 20)
+        Me.NumericUpDown2.TabIndex = 6
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 89)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(232, 137)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Statistics"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label7)
+        Me.GroupBox4.Controls.Add(Me.GroupBox6)
+        Me.GroupBox4.Controls.Add(Me.GroupBox5)
+        Me.GroupBox4.Location = New System.Drawing.Point(14, 60)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(204, 71)
+        Me.GroupBox4.TabIndex = 2
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Coin Surplus/Deficit"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 16)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(27, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Nan"
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.Label9)
+        Me.GroupBox6.Location = New System.Drawing.Point(103, 32)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(95, 31)
+        Me.GroupBox6.TabIndex = 1
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "Weight (mg)"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(6, 16)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 0
+        Me.Label9.Text = "Label9"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.Label8)
+        Me.GroupBox5.Location = New System.Drawing.Point(6, 32)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(88, 32)
+        Me.GroupBox5.TabIndex = 0
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Coins"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 16)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Label8"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Location = New System.Drawing.Point(117, 20)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(101, 34)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Ideal weight (mg)"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(7, 15)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(27, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Nan"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 19)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(98, 35)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Number of coins"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(7, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(27, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Nan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 242)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Lukasz Baldyga"
+        '
+        'Engine
+        '
+        Me.Engine.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(261, 264)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = " "
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox6.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Engine As Timer
 End Class
--- Coin-Problem/Coin-Problem/Form1.vb
+++ Coin-Problem/Coin-Problem/Form1.vb
@@ -1,3 +1,83 @@
 ﻿Public Class Form1
+    Public Function GetWeight(CoinType) 'This means that this function will get the weight (in miligrams) from a coin type.
+        If CoinType = "1p" Then
+            Return 356 ' this is the weight of 1p's
+            'Return basically means that the code will give out this number when this function is ran. 
+        ElseIf CoinType = "2p" Then
+            Return 356 'this is the weight of 2p's
+        ElseIf CoinType = "5p" Then
+            Return 325 'this is the weight of 5p's
+        ElseIf CoinType = "10p" Then
+            Return 325 'this is the weight of 10p's
+        ElseIf CoinType = "20p" Then
+            Return 250 'this is the weight of 20p's
+        ElseIf CoinType = "50p" Then
+            Return 160 'this is the weight of 50p's
+        ElseIf CoinType = "£1" Then
+            Return 190 'this is the weight of £1's
+        ElseIf CoinType = "£2" Then
+            Return 120 'this is the weight of £2's
+        Else
+            Return Nothing 'If it doesn't find the coin, it gives out nothing. (Avoids Null Errors)
+        End If
 
+    End Function
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Engine.Start() 'This about this like your car engine. Start it by igniting it.Then the engine does 4 stages:
+        'it takes in fuel + air, compresses it, ignies it and gives out the waste. These stages repeat to drive your car. 
+        'Think of the engine like this. It will do a load of calculations at a time. Every cycle it will take in the
+        'numbers and generate the statistics. This is good because the program doesn't really need a trigger 
+        '(eg a click or a change) in every object. This would make the code a mess. Maybe even dependant a single function
+        'doing all the work. It would be desaster and living hell to refactor!! (trust me)
+        'Everything Is commented below. 
+    End Sub
+    Private Sub Engine_Tick(sender As Object, e As EventArgs) Handles Engine.Tick
+        Dim WeightPerCoin = GetWeight(ComboBox1.Text)
+
+        NumericUpDown2.Increment = WeightPerCoin                    'Cool feature B-)
+
+        If WeightPerCoin <> Nothing Then
+            Dim IdealWeight = WeightPerCoin * NumericUpDown1.Value  'Ideal weight of the coins.
+            Label6.Text = Str(IdealWeight)                          'Asignment of value
+
+            Dim NumberOfCoins = Int(Math.Floor(NumericUpDown2.Value \ WeightPerCoin))
+            'devision can make numbers NASTY!! However, it should be safe since primes in
+            'WeightPerCoin() are 2 and 5. Souldn't be any reccuring decimals. It doesn't matter because
+            'the numbers will be rounded down to the lowest int. (Math.Floor() "floors a float")
+            Label5.Text = Str(NumberOfCoins)
+
+            Dim SDWeight = IdealWeight - NumericUpDown2.Value 'Ideal weight - Actual Weight
+
+            Dim SDCoins = ((NumericUpDown1.Value - NumberOfCoins) ^ 2) ^ 0.5 'Gets rid of possible minus. (because -1 * -1 is 1)
+            Label8.Text = SDCoins
+
+            If SDWeight > NumericUpDown1.Value Then
+                Label7.Text = "Deficit Of"
+                Label9.Text = Str(SDWeight)
+            ElseIf SDWeight < NumericUpDown1.Value Then
+                Label7.Text = "Surplus of"
+                SDWeight = SDWeight * -1 '*-1 because it would be negative.
+                Label9.Text = Str(SDWeight)
+            End If
+            If SDCoins = 0 Then
+                Label7.Text = "Just right"
+                Label8.Text = ""
+                Label9.Text = ""
+            End If
+
+
+        Else
+            'RAISE ERROR D: (Though, this really should be impossible to get to... smh if you do.)
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        'If Changed coin type, reset weight to zero.
+        NumericUpDown2.Value = 0
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim webAddress As String = "https://github.com/c3ypt1c/Coin-Problem"
+        Process.Start(webAddress)  'This is just me linking this program to github :D
+    End Sub
 End Class
